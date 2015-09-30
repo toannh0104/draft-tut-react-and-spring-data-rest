4928f547f3453afc292097e70c06447e26837ca8
Greg Turnquist <gturnquist@pivotal.io>
2015-09-30 11:51:09 -0500

    Polish

--- es6/src/main/resources/static/app.js
+++ es6/src/main/resources/static/app.js
@@ -16,7 +16,6 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log('Initializing state...');
         this.state = {
             page: {},
             employees: [],
