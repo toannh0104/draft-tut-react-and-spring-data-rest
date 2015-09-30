912c0971415680a8f2ef1c47d0a79aa5e2b80959
Greg Turnquist <gturnquist@pivotal.io>
2015-09-30 10:35:38 -0500

    Alias submodules

--- es6/src/main/resources/static/app.js
+++ es6/src/main/resources/static/app.js
@@ -4,7 +4,7 @@ console.log('Babel was here');
 
 var React = require('react');
 //var when = require('when');
-//var client = require('./client');
+var client = require('./client');
 //var follow = require('./follow');
 //
 //var stompClient = require('./websocket-listener');
