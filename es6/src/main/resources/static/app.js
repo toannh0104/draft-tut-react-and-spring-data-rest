963197a403f9c18c41ef773daafe09b3693a9001
Greg Turnquist <gturnquist@pivotal.io>
2015-09-30 10:01:55 -0500

    Put react back in and comment out custom

--- es6/src/main/resources/static/app.js
+++ es6/src/main/resources/static/app.js
@@ -3,8 +3,8 @@
 console.log('Babel was here');
 
 var React = require('react');
-var when = require('when');
-var client = require('./client');
+//var when = require('when');
+//var client = require('./client');
 //var follow = require('./follow');
 //
 //var stompClient = require('./websocket-listener');
